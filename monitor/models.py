99be8241e7ba11300f28c574c71219ac79927464
omniaura <omniaura5@gmail.com>
2014-01-31 17:43:47 -0800

    added models

--- monitor/models.py
+++ monitor/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class UserSite(models.Model):
+    user = models.ForeignKey('User')
+    host = models.CharField(max_length=253) # max length of a domain name
+
+    def __unicode__(self):
+        return "%s: %s" % (self.user, self.host)
+
+
+class LogEntry(models.Model):
+    site = models.ForeignKey(UserSite)
+    # status_code = models.CharField(max_length=3)
+    source_url = models.URLField()
+    destination_url = models.URLField()
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return "%s > %s" % (self.source_url, self.destination_url)
