e3a0630b1ef01ddd77e5c53f4b52172d813fa491
omniaura <omniaura5@gmail.com>
2014-01-31 20:49:30 -0800

    implemented optional callback, selector include/exclude, and origin settings

--- monitor/static/js/404.js
+++ monitor/static/js/404.js
@@ -1,16 +1,51 @@
 $(document).ready(function(){
+
     var source = document.URL;
-    $('a').click(function(event){
-        event.preventDefault();
+
+    /* configure settings */
+    if (typeof _404_SETTINGS == 'undefined') _404_SETTINGS = {};
+    if (!_404_SETTINGS.origin) _404_SETTINGS.origin = 'both';
+
+    if (typeof _404_SETTINGS.include == 'undefined') 
+        _404_SETTINGS.include = 'a';
+    if (typeof _404_SETTINGS.exclude == 'undefined') 
+        _404_SETTINGS.exclude = '';
+    var selector = $(_404_SETTINGS.include).not(_404_SETTINGS.exclude);
+    /* end settings */
+
+    selector.click(function(event){
         var destination = $(this).prop('href');
+        // don't do anything else if the origin setting doesn't match
+        if (_404_SETTINGS.origin == 'different' && sameOrigin(destination))
+            return;
+        else if (_404_SETTINGS.origin == 'same' && !sameOrigin(destination))
+            return;
+        event.preventDefault();
         ajaxGet(
             {source: source, destination: destination},
             'http://localhost:8000/api/check/',
-            function(response){ console.log(response); window.location = destination; }
+            function(response) {
+                if (response.error404 && _404_SETTINGS.callback)
+                    _404_SETTINGS.callback;
+                else
+                    console.log(response);
+            }
         );
-        // window.location = destination;
+        if (!_404_SETTINGS.callback)
+            window.location = destination;
     });
+
 });
+
+
+/* utility functions */
+
+function sameOrigin(url) {
+    var link = document.createElement('a'); // TODO: make sure this doesn't cause a memory leak
+    link.href = url;
+    if (location.host == link.host) return true;
+    else return false;
+}
 
 function ajaxGet(params, endpoint, callback_success) {
     $.ajax({
