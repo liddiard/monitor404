a041f426a5cace840230edd6ba0bd6e5d781971c
omniaura <omniaura5@gmail.com>
2014-01-31 17:46:48 -0800

    registered models in admin

--- monitor/admin.py
+++ monitor/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import UserSite, LogEntry
 
-# Register your models here.
+
+class UserSiteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'host')    
+
+
+admin.site.register(UserSite, UserSiteAdmin)
+
+
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ('site', 'source_url', 'destination_url', 'time')
+
+
+admin.site.register(LogEntry, LogEntryAdmin)
