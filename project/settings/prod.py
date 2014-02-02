a2d18a7b610663b0628c9655b78f818b4c879cb0
omniaura <omniaura5@gmail.com>
2014-02-02 14:04:22 -0800

    changed production db settings for ec2

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -4,7 +4,15 @@ import dj_database_url
 DEBUG = True
 
 DATABASES = {
-    'default': dj_database_url.config()    
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '404monitor',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'ec2-user',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
 }
 
 AWS_STORAGE_BUCKET_NAME = '404monitor'
