563ba168f132ed46f735ecee4c9d93c391ca1879
omniaura <omniaura5@gmail.com>
2014-02-01 18:58:28 -0800

    renamed actual url of 'log' to 'dashboard'

--- project/urls.py
+++ project/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
 
     # pages
     url(r'^$', views.FrontView.as_view(), name='front'),
-    url(r'^log/(?P<slug>\S+)?$', views.LogView.as_view(), name='log'),
+    url(r'^dashboard/(?P<slug>\S+)?$', views.LogView.as_view(), name='log'),
     url(r'^demo/$', views.DemoView.as_view(), name='demo'),
 
     # admin
