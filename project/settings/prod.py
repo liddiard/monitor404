46c2222a37500dc48ed52d1557167a7357e9d3ff
omniaura <omniaura5@gmail.com>
2014-02-01 19:04:06 -0800

    changed production STATIC_URL to point to s3

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -6,3 +6,8 @@ DEBUG = False
 DATABASES = {
     'default': dj_database_url.config()    
 }
+
+AWS_STORAGE_BUCKET_NAME = '404monitor'
+S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+
+STATIC_URL = S3_URL
