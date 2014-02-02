01fb571da9512df361bb4338d8aa87bd3e03e80d
omniaura <omniaura5@gmail.com>
2014-02-01 20:09:31 -0800

    added missing import to Http404 in view

--- monitor/views.py
+++ monitor/views.py
@@ -1,6 +1,6 @@
 import urllib2
 import json
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.views.generic.base import View, TemplateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
