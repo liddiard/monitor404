be462ab6f8a3a42c5753d1eec91d4216a4ca923e
omniaura <omniaura5@gmail.com>
2014-02-10 16:50:38 -0800

    fixed celery constructor argument

--- project/celery.py
+++ project/celery.py
@@ -9,7 +9,7 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.local')
 
-app = Celery('proj')
+app = Celery('project')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
