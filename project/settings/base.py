0228729d4e21328ae729a76325f061f602517795
omniaura <omniaura5@gmail.com>
2014-02-11 09:11:06 -0800

    added djcelery setup_uploader to settings

--- project/settings/base.py
+++ project/settings/base.py
@@ -8,8 +8,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
+import djcelery
+djcelery.setup_uploader()
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
