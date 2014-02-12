f16d31f08db7db6bb66250c94b7dc7debd48975e
omniaura <omniaura5@gmail.com>
2014-02-11 20:08:13 -0800

    commented out celery import in settings

--- project/settings/base.py
+++ project/settings/base.py
@@ -10,8 +10,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 import os
 
-import djcelery
-djcelery.setup_uploader()
+# import djcelery
+# djcelery.setup_uploader()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
