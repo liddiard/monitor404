1843ebebe9ff3457b2e14b5ca97510799c262c5b
omniaura <omniaura5@gmail.com>
2014-02-13 19:46:30 -0800

    added send_error_email func

--- monitor/tasks.py
+++ monitor/tasks.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import
 import urllib2
 
+from django.core.urlresolvers import reverse
+from django.core.mail import send_mail
+
 # from celery import shared_task
 from celery.task import task
 
@@ -19,6 +22,40 @@ def is_404(url):
     else:
         return False
 
+def send_error_email(site, source, destination):
+    email_message = '''
+        Hi %(username)s,
+
+        404monitor detected a HTTP 404 error on %(site)s.
+
+        A user on the page: %(source)s
+        clicked on a link to: %(destination)s,
+        which resulted in the error.
+
+        For more information, check the %(site)s dashboard on 404monitor:
+        %(dashboard_url)s.
+
+        Thanks,
+        The 404monitor team
+    '''
+
+    context = {
+        'username': site.user.username,
+        'site': site.host,
+        'source': source,
+        'destination': destination,
+        'dashboard_url': reverse('log', args=(site.slug,))
+    }
+
+    send_mail(
+        '404monitor: 404 Error Detected on %s' % site,
+        email_message % context,
+        'notification@404monitor.io',
+        [user.email],
+        fail_silently=False,
+        html_message=None
+    )
+
 
 # tasks
 
@@ -36,9 +73,10 @@ def check_404(source, destination, sites):
             error, created = LogEntry.objects.get_or_create(site=site, 
                                                          source_url=source, 
                                                destination_url=destination)
-            if not created: # it already existed
+            if created:
+                send_error_email(site, source, destination)
+            else: # it already existed
                 error.save()
-        # send an email
         return 1 # url 404'd!
     else:
         return 0 # check performed b/c it wasn't cached
