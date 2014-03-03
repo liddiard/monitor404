801b9751252a4a4629cd9c7610a2c4b7a308d1b1
omniaura <omniaura5@gmail.com>
2014-03-02 20:27:05 -0800

    turn REGISTRATION OFF in production

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -7,10 +7,17 @@ DATABASES = {
     'default': dj_database_url.config()
 }
 
+REGISTRATION_OPEN = False
+
+# static files
+
 AWS_STORAGE_BUCKET_NAME = 'monitor404'
 S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 
 STATIC_URL = S3_URL
+
+
+# email
 
 MANDRILL_API_KEY = os.environ['MANDRILL_API_KEY']
 EMAIL_BACKEND = "djrill.mail.backends.djrill.DjrillBackend"
