7cad8bf27fc97ecdd8d3401faa1e8442f0e879b2
omniaura <omniaura5@gmail.com>
2014-03-14 09:54:46 -0700

    added print statements ahead of all return values for debug logging

--- monitor/tasks.py
+++ monitor/tasks.py
@@ -106,6 +106,7 @@ def check_404(source, destination, sites):
             eligible_site = True
             break
     if not eligible_site:
+        print -3
         return -3 # no eligible sites
 
     destination_host = urlparse(destination).netloc
@@ -115,6 +116,7 @@ def check_404(source, destination, sites):
             site_to_skip = True
             break
     if site_to_skip:
+        print -2
         return -2 # site is stored in db as one to skip
 
     check, created = URLCheck.objects.get_or_create(url=destination)
@@ -123,6 +125,7 @@ def check_404(source, destination, sites):
             check.save() # update the check with the current time
                          # because we're going to check it next...
         else:
+            print -1
             return -1 # url check is fresh; no further processing required
 
     if is_404(destination):
@@ -143,6 +146,8 @@ def check_404(source, destination, sites):
                     prefs = UserPrefs.objects.get_or_create(user=site.user)[0]
                     if prefs.email_quota:
                         send_quota_email(site)
+        print 1
         return 1 # url 404'd!
     else:
+        print 0
         return 0 # check performed b/c it wasn't cached
