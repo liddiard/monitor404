0edd408d03695f9d4ada54675b437fe9d85277dd
omniaura <omniaura5@gmail.com>
2014-03-14 09:43:56 -0700

    explicitly specify https-only url for api access

--- monitor/static/js/monitor404.js
+++ monitor/static/js/monitor404.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
         var timer_id;
         ajaxGet(
             {source: source, destination: destination},
-            '//monitor404.com/api/check/',
+            'https://monitor404.com/api/check/',
             function(response) {
                 if (timer_id)
                     window.clearTimeout(timer_id);
